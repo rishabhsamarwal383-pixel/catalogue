7823de8f0d6b38d16fc77d8f29ae2243b36e0ade
rishabhsamarwal383-pixel <rishabhsamarwal383@gmail.com>
2026-09-25 17:58:22 +0530

    ⚡ Auto-deploy: Updated sw.js [Cloud Sync]

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Box Company Udaipur - PWA Service Worker (Auto-Update & Push Notifications)
-const CACHE_NAME = 'box-co-v2';
+const CACHE_NAME = 'box-co-v3';
 const ASSETS_TO_CACHE = [
   './',
   './manifest.json',
